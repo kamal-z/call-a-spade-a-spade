77bc7e14003b02b9554835584f10985df93e91d2
kamal <kamalzalagh@gmail.com>
2022-05-10 14:59:02 +0200

    first commit

---
+++ src/main/java/com/callaspadeaspade/CallASpadeASpadeApplication.java
@@ -0,0 +1,13 @@
+package com.callaspadeaspade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallASpadeASpadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallASpadeASpadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callaspadeaspade/CallASpadeASpadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callaspadeaspade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallASpadeASpadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
